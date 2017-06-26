1b4436de6293623570adb4ebfc6347ef2540a3dd
Kacper <kacperadach@gmail.com>
2017-06-26 17:09:17 -0400

    historical stock acquisition works

--- acquisition/historical/stocks.py
+++ acquisition/historical/stocks.py
@@ -16,45 +16,51 @@ class HistoricalStockAcquisition():
         self.finance_db = FinanceDB('stock_historical')
         self.symbols = StockTickers().get_all()
         self.counter = 0
+        self.date = None
 
     def next(self):
+        if self.date is None:
+            self.date = datetime.now().date()
         now = datetime.now().date()
-        today = datetime(year=now.year, month=now.month, day=now.day)
-        yesterday = today - timedelta(days=1)
-        self.current_symbol = self.symbols[self.counter]
+        yesterday = datetime(year=now.year, month=now.month, day=now.day) - timedelta(days=1)
+        if self.counter < len(self.symbols):
+            self.current_symbol = self.symbols[self.counter]
+        else:
+            self.counter = 0
+            raise StopIteration
         stock_record = self.schedule_db.query(HistoricalStockData, {'symbol': self.current_symbol}).first()
+
+        if stock_record is None:
+            start = yesterday - timedelta(days=DAYS_PER_CALL)
+            end = yesterday
+        else:
+            end_date = datetime(year=stock_record.end_date.year, month=stock_record.end_date.month, day=stock_record.end_date.day)
+            start = end_date + timedelta(days=1)
+            end = yesterday
+        data = QuoteNetworking(symbol=self.current_symbol, start=start, end=end, log_process=False).get_data()
+        minute_data = filter(lambda (k, v): 'data' in v.keys() and len(v['data']) >= 390, data.iteritems())
+        minute_data = dict(sorted(minute_data, key=lambda x: x[0]))
+        documents = []
+
+        start,end = None, None
+        for key, data in minute_data.iteritems():
+            if start is None or key.date() < start:
+                start = key.date()
+            if end is None or key.date() > end:
+                end = key.date()
+            data['trading_date'] = str(key.date())
+            data['symbol'] = self.current_symbol
+            documents.append(data)
+
         if stock_record is None:
             stock_record = HistoricalStockData(symbol=self.current_symbol)
-            data = QuoteNetworking(symbol=self.current_symbol, start=yesterday - timedelta(DAYS_PER_CALL), end=yesterday, log_process=False).get_data()
-            minute_data = filter(lambda (k,v): 'data' in v.keys() and len(v['data']) >= 390, data.iteritems())
-            minute_data = dict(sorted(minute_data, key=lambda x: x[0]))
-            documents = []
-            start, end = None, None
-            for key, data in minute_data.iteritems():
-                if start is None or key.date() < start:
-                    start = key.date()
-                if end is None or key.date() > end:
-                    end = key.date()
-                data['trading_date'] = key.date()
-                documents.append(data)
             stock_record.start_date = start
-            stock_record.end_date = end
             stock_record.minute_start_date = start
             stock_record.has_minute_granularity = True if start and end else False
+
+        if end and documents:
+            stock_record.end_date = end
             self.finance_db.insert_many(documents)
             self.schedule_db.add_to_schedule(stock_record)
-        else:
-            pass
 
-
-h =  HistoricalStockAcquisition()
-h.next()
-a = 1
-
-
-
-
-
-
-
-
+        self.counter += 1
