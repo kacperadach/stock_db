665bf535c57a85d6020487dca97afc9b7a7c4a0e
kacperadach <kacperadach@gmail.com>
2019-06-19 21:21:31 -0400

    work

--- api/application.py
+++ api/application.py
@@ -9,12 +9,15 @@ app = Flask(__name__, static_folder='react_app/build')
 app.register_blueprint(quote_view, url_prefix="/quote")
 app.register_blueprint(symbols_view, url_prefix="/symbols")
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # Serve React App
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve(path):
     print path
-    if path != "" and os.path.exists("react_app/build/" + path):
+    print dir_path
+    if path != "" and os.path.exists(os.path.join(dir_path,'react_app/build', path)):
         return send_from_directory('react_app/build', path)
     else:
         return send_from_directory('react_app/build', 'index.html')
