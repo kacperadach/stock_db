2a0b3e07a707cfa4be8f6d7a8c4517d95b6fd75c
kacperadach <kacperadach@gmail.com>
2019-07-12 19:36:49 -0400

    work

--- app/constants.py
+++ app/constants.py
@@ -1,11 +1,11 @@
 DEV_ENV_VARS = {
     'FINANCE_DB_NAME': 'finance_dev',
-    'FINANCE_DB_HOST': 'www.bullzone.club',
+    'FINANCE_DB_HOST': 'localhost',
     'FINANCE_DB_PORT': '27017'
 }
 
 PROD_ENV_VARS = {
     'FINANCE_DB_NAME': 'finance_prod',
-    'FINANCE_DB_HOST': 'www.bullzone.club',
+    'FINANCE_DB_HOST': 'localhost',
     'FINANCE_DB_PORT': '27017'
 }
