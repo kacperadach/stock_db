5a4ee98357bf032a9f175c75ed642aa4ab9114ac
kacperadach <kacperadach@gmail.com>
2019-08-08 18:00:36 -0400

    work

--- acquisition/scrapers/FuturesScraper.py
+++ acquisition/scrapers/FuturesScraper.py
@@ -32,11 +32,14 @@ class FuturesScraper(BaseScraper):
         return timedelta(minutes=4)
 
     def process_data(self, queue_item):
+        self.log('processing data')
         data = MarketWatchRequest.parse_response(queue_item.get_response().get_data())
+        self.log('parsed response')
         if not data:
             self.log('Future not found: {}'.format(queue_item.get_metadata()))
 
         if data:
+            self.log('inserting data')
             self.quote_repository.insert(data, queue_item.get_metadata())
 
 
