91269aa7bc335773ddb39c1aad1d4d036bb48330
kacperadach <kacperadach@gmail.com>
2019-11-02 13:06:46 -0400

    errors

--- core/OutputWorkerProcess.py
+++ core/OutputWorkerProcess.py
@@ -10,11 +10,13 @@ from logger import AppLogger
 from acquisition.scrapers import ALL_SCRAPERS
 
 def output_worker_process(process_queue, log_file_name):
-    logger = AppLogger()
-    logger.log('Started Output Process, pid: {}'.format(os.getpid()))
+    # logger = AppLogger()
+    # logger.log('Started Output Process, pid: {}'.format(os.getpid()))
+    print 'Started worker process'
     try:
         while 1:
             try:
+                print 'fetching from process queue'
                 queue_item = process_queue.get(block=False)
             except Empty:
                 sleep(0.1)
@@ -28,21 +30,24 @@ def output_worker_process(process_queue, log_file_name):
                     scraper = s()
             if scraper is None:
                 e = RuntimeError('could not find scraper: {}'.format(callback_scraper))
-                logger.log(traceback.format_exc(e))
+                # logger.log(traceback.format_exc(e))
+                print 'could not find scraper'
                 raise e
             try:
                 scraper.process_data(queue_item)
             except Exception as e:
-                logger.log('PROCESSING ERROR FATAL: {} - {}'.format(scraper.__name__, e))
+                print 'error occurred while processing data'
+                # logger.log('PROCESSING ERROR FATAL: {} - {}'.format(scraper.__name__, e))
 
             seconds_took = (datetime.utcnow() - start).total_seconds()
-            logger.log('Output processing: - took {} seconds: {}'.format(seconds_took, queue_item.get_metadata()))
+            # logger.log('Output processing: - took {} seconds: {}'.format(seconds_took, queue_item.get_metadata()))
 
             # if seconds_took > 5:
             # logger.log('Slow output processing for metadata: {} - took {} seconds'.format(queue_item.get_metadata(), seconds_took))
     except Exception as e:
-        logger.log('ERROR ERROR ERROR: {}'.format(e), level='error')
-        logger.log(traceback.format_exc(e))
+        # logger.log('ERROR ERROR ERROR: {}'.format(e), level='error')
+        # logger.log(traceback.format_exc(e))
+        print 'unexpected error occurred'
         raise e
 
 if __name__ == "__main__":
